bee5a60369cc15bede22a9463bcdaa4844603c4f
RileyWClarke <rclarke1995@gmail.com>
2022-04-15 22:01:03 -0400

    4/15 update

--- ztf_shift_script.py
+++ ztf_shift_script.py
@@ -15,9 +15,9 @@ print(" ")
 print("--------------------------")
 print("Importing image files...")
 
-getsciimg(filefracday = scistring[0:14], paddedfield = scistring[14:20],filtercode = scistring[20:22],
-          paddedccdid = scistring[22:24],imgtypecode = scistring[24], qid = scistring[25], flip=False)
-getrefimg(paddedfield = refstring[0:6],filtercode = refstring[6:8], paddedccdid = refstring[8:10], qid = refstring[10])
+getsciimg(filefracday = scistring[0:14], paddedfield = scistring[14:20], filtercode = scistring[20:22],
+          paddedccdid = scistring[22:24], imgtypecode = scistring[24], qid = scistring[25], flip=False)
+getrefimg(paddedfield = refstring[0:6], filtercode = refstring[6:8], paddedccdid = refstring[8:10], qid = refstring[10])
 
 ##Generate srcext catalogues
 print(" ")
@@ -38,6 +38,7 @@ with fits.open('srcext/'+refstring[0:6]+'_refimg.fits') as hdu:
 print(" ")
 print("--------------------------")
 print("Running Astropy x-match...")
+
 ind, d2d, d3d = xmatch(sci_cata,ref_cata)
 
 seps = d2d.to(u.arcsec).value
@@ -47,21 +48,17 @@ ref_coord = SkyCoord(ref_cata["ALPHA_SKY"][ind].values * u.degree,
 sci_coord = SkyCoord(sci_cata["ALPHA_SKY"].values * u.degree, 
                 sci_cata["DELTA_SKY"].values * u.degree, frame='icrs')
 
+
 #Calculate zenith direction
 zenith = calc_zenith(date)
-
 theta_1 = ref_coord.position_angle(zenith).arcsecond
 theta_2 = ref_coord.position_angle(sci_coord).arcsecond
 
 good_sep = seps < 1
-
-proj = seps[good_sep] * np.cos(theta_1[good_sep] - theta_2[good_sep])
-
+#proj = seps[good_sep] * np.cos(theta_1[good_sep] - theta_2[good_sep])
 am  = sci_header[97]
-print(sci_header)
 
-##Find flare star
-
+#Find flare star
 f_ind = np.where(np.abs(sci_cata[["ALPHA_SKY","DELTA_SKY"]][good_sep].values - [flr_ra, flr_dec]) == np.abs(sci_cata[["ALPHA_SKY","DELTA_SKY"]][good_sep].values - [flr_ra, flr_dec]).min())[0][0]
 
 print(" ")
--- ztf_shift_utils.py
+++ ztf_shift_utils.py
@@ -12,7 +12,9 @@ from astropy.utils.data import get_pkg_data_filename
 from astropy.io import fits
 from astropy.wcs import WCS
 
-def getsciimg(filefracday,paddedfield,filtercode,paddedccdid,imgtypecode,qid, flip=True):
+from astroquery.gaia import Gaia
+
+def getsciimg(filefracday,paddedfield,filtercode,paddedccdid,imgtypecode,qid):
 
     year = filefracday[0:4]
     month = filefracday[4:6]
@@ -24,7 +26,17 @@ def getsciimg(filefracday,paddedfield,filtercode,paddedccdid,imgtypecode,qid, fl
     hdu = fits.open(url)
     hdu.writeto('srcext/'+filefracday+'_'+paddedfield+'_sciimg'+'.fits', overwrite=True)
 
+def getgaiacat(sra,sdec,srad,verb=2):
+    
+    Gaia.ROW_LIMIT = 200 
+    coord = SkyCoord(ra=sra, dec=sdec, unit=(u.degree, u.degree), frame='icrs')
+    radius = u.Quantity(srad, u.deg)
+    j = Gaia.cone_search_async(coord, radius)
+    r = j.get_results()
+   
+    gaia_df = pd.DataFrame({'ra':r['ra'], 'dec':r['dec'], 'gmag':r['phot_g_mean_mag'], 'rmag':r['phot_rp_mean_mag']})
 
+    return gaia_df
 
 def getrefimg(paddedfield,filtercode,paddedccdid,qid):
 
@@ -75,6 +87,25 @@ def xmatch(cat1, cat2):
 
     return idx, d2d, d3d
 
+def xmatch_gaia(cat1, cat2):
+
+    c1 = SkyCoord(ra=cat1["ALPHA_SKY"]*u.degree, dec=cat1["DELTA_SKY"]*u.degree)
+    c2 = SkyCoord(ra=cat2["ra"].values*u.degree, dec=cat2["dec"].values*u.degree)
+    idx, d2d, d3d = c1.match_to_catalog_sky(c2)
+
+    return idx, d2d, d3d
+
+def circle_cut(imgcen_ra, imgcen_dec, cat, radius):
+    '''
+    Returns indices from cat within radius of imgcenter
+    '''
+    
+    c1 = SkyCoord(ra=imgcen_ra*u.degree, dec=imgcen_dec*u.degree)
+    c2 = SkyCoord(ra=cat["ra"].values*u.degree, dec=cat["dec"].values*u.degree)
+    ind = c1.separation(c2) < radius
+
+    return ind
+
 def calc_zenith(date):
 
     mtn = EarthLocation.of_site('Palomar')
@@ -100,8 +131,8 @@ def plot_shifts(ref_ra, ref_dec, sci_ra, sci_dec, zen_ra, zen_dec, flr_ind, mjd,
     #Plotting
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(d_ra,d_dec, alpha=0.5)
-    ax.scatter(d_ra[flr_ind], d_dec[flr_ind], color='r', label='Flare star')
+    ax.scatter(d_ra, d_dec, alpha=0.75)
+    ax.scatter(d_ra[flr_ind], d_dec[flr_ind], color='red', s=100, marker='*', label='Flare star')
 
     if not centered:
         ax.scatter(centroid[0], centroid[1], color='k', marker='x', label="Centroid")
@@ -133,4 +164,62 @@ def plot_shifts(ref_ra, ref_dec, sci_ra, sci_dec, zen_ra, zen_dec, flr_ind, mjd,
     ax.set_title("MJD: {0}, Airmass = {1}".format(mjd, am))
     ax.legend()
     ax.grid(False)
+    
     plt.gca().set_aspect('equal')
+
+def plot_shifts_gaia(ref_ra, ref_dec, sci_ra, sci_dec, zen_ra, zen_dec, flr_ind, mjd, am, circ_inds, colors, centered=False):
+
+    #Calc delta coords
+    d_ra = (ref_ra - sci_ra) * 3600
+    d_dec = (ref_dec - sci_dec) * 3600
+
+    #Calc zenith delta coord
+    d_zra = (ref_ra.mean() - zen_ra) * 3600
+    d_zdec = (ref_dec.mean() - zen_dec) * 3600
+
+    #Calc centroid
+    centroid = (sum(d_ra) / len(d_ra), sum(d_dec) / len(d_dec))
+
+    #Plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    inners = ax.scatter(d_ra,d_dec, c = colors, cmap='RdBu_r', alpha=0.75)
+    #outers = ax.scatter(d_ra[~circ_inds],d_dec[~circ_inds], facecolors='none', edgecolors='black', label='peripheral (>0.5 deg)', alpha=0.5)
+    ax.scatter(d_ra[flr_ind], d_dec[flr_ind], color='chartreuse', s=100, marker='*', label='Flare star')
+
+    if not centered:
+        ax.scatter(centroid[0], centroid[1], color='k', marker='x', label="Centroid")
+
+    ax.plot([0,d_zra],[0,d_zdec], c='gray',ls='--', label="to zenith")
+
+    # Move left y-axis and bottim x-axis to centre, passing through (0,0)
+
+    if centered:
+        ax.spines['left'].set_position(('data', centroid[0]))
+        ax.spines['bottom'].set_position(('data', centroid[1]))
+    else:
+        ax.spines['left'].set_position('center')
+        ax.spines['bottom'].set_position('center')
+
+    # Eliminate upper and right axes
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    # Show ticks in the left and lower axes only
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    cbar = plt.colorbar(inners)
+    cbar.set_label('Gaia g-rp (mag)', rotation=270, labelpad=20)
+
+    ax.set_xlabel(r"$\Delta$ RA (arcsec)", labelpad=150)
+    ax.set_ylabel(r"$\Delta$ Dec (arcsec)", labelpad=150)
+    ax.set_xticks([-0.4,-0.2,0.2,0.4])
+    ax.set_xlim(-0.5,0.5)
+    ax.set_ylim(-0.5,0.5)
+    ax.set_title("MJD: {0}, Airmass = {1}".format(mjd, am))
+    ax.legend()
+    ax.grid(False)
+    
+    plt.gca().set_aspect('equal')
+   
