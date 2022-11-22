b45bc61ac6868fade4a350b7ac58276d209fdbb0
rileywclarke <rclarke1995@gmail.com>
2022-11-21 19:14:02 -0500

    Fixed broken path string

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from astropy.coordinates import EarthLocation
 WAVELENGTH = np.arange(0,12001,1)
 WMIN = 3825
 WMAX = 9200
-MDSPEC = 'm5.active.ha.na.k.fits'
+MDSPEC = '/Users/riley/Desktop/RAFTS/m5.active.ha.na.k.fits'
 AMS = np.linspace(1.05,1.2,num=6,dtype='float')
 
 deg2rad = np.pi / 180
