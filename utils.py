349873dd08c05f519d5e78549cae5408bff6faa5
federica <fedhere@gmail.com>
2025-09-05 13:57:03 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ import scipy
 from scipy.interpolate import interp1d
 from scipy.optimize import minimize
 
+import rubin_sims
 import rubin_sim.phot_utils.bandpass as Bandpass
 #import rubin_sim.phot_utils.Sed as Sed
 
